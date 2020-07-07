84f3fab4bfc1b25e998ee4f10845d0c01aab279b
Sahal Asghar <sahaldos1@gmail.com>
2020-07-07 12:19:05 -0400

    created articles table in database

--- db/migrate/20200707160834_create_articles.rb
+++ db/migrate/20200707160834_create_articles.rb
@@ -1,8 +1,14 @@
 class CreateArticles < ActiveRecord::Migration[5.2]
-  def change
+  def change 
+    #create a table articles, t is the variable that refernces the table that's created. We call methods on t to create columns in the articles table.
     create_table :articles do |t|
+      t.string :title
+      t.text :body
+
 
       t.timestamps
+
+      #after saving a migration file run bin/rake db:migrate to run maintenance functions on your app. migrate runs any migrations that havn't been run.
     end
   end
 end
