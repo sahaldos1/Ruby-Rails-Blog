28462845bf6f69f0004ce4a0f30c47b1897cc292
Sahal Asghar <sahaldos1@gmail.com>
2020-07-07 12:41:17 -0400

    added articles controller and basic view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  #tells rails that we have a resource named articles
+  resources :articles
 end
